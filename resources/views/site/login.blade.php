c37fd2a8102f240254b0caa29c2c4190f7c7ee60
Isac Fernandes <isac.costa@fatec.sp.gov.br>
2022-08-18 13:46:31 -0300

    changes

--- resources/views/site/login.blade.php
+++ resources/views/site/login.blade.php
@@ -1,1 +1,12 @@
 <h3> Login page </h3>
+<ul>
+    <li>
+        <a href="{{ route('site.index') }}"> Homepage </a>
+    </li>
+    <li>
+        <a href="{{ route('site.login') }}"> Login </a>
+    </li>
+    <li>
+        <a href="{{ route('app.admin') }}"> Admin </a>
+    </li>
+</ul>
