138fbe8b33271450516646bb932e002496e2f3cd
Isac Fernandes <isac.costa@fatec.sp.gov.br>
2022-08-18 10:49:06 -0300

    changes

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [\App\Http\Controllers\HomepageController::class, 'homepage']);
+Route::get('/', [\App\Http\Controllers\HomepageController::class, 'homepage'])->name('site.index');
 
+Route::get('/login', [\App\Http\Controllers\LoginController::class, 'login'])->name('site.login');
 
-Route::get('/login', [\App\Http\Controllers\LoginController::class, 'login']);
-
-Route::get('/login/{nome}', function ($nome) {
-    echo "Login realizado com sucesso! <br/>";
-    echo "Seja bem-vindo ".$nome;
+Route::prefix('/app')->group(function() {
+    Route::get('/admin', [\App\Http\Controllers\AdminController::class, 'admin'])->name('app.admin');
 });
 
-Route::get('/admin', [\App\Http\Controllers\AdminController::class, 'admin']);
+/*
+Route::get('/login/{nome}/{mensagem?}', function (string $nome, string $mensagem = 'mensagem não atribuida!') {
+    echo "Login realizado com sucesso! <br/>";
+    echo "Seja bem-vindo ".$nome."<br/>";
+    echo "Essa é sua mensagem: ".$mensagem;
+});
+*/
+/*
+Ao colocar a '?' após a variavel ela será uma variavel opcional;
+No caso abaixo, as variaveis deverão ser passadas ou ocorrerá um erro;
+Route::get('/login/{nome}/{idade}', function (string $nome, int $idade) {
+    echo "Login realizado com sucesso! <br/>";
+    echo "Seja bem-vindo ".$nome;
+    echo "Você tem ".$idade. " anos?";
+});
+*/
