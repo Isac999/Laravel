376d8335e9d486c3af3da7f9b8b64b0bdf31dda7
Isac Fernandes <isac.costa@fatec.sp.gov.br>
2022-08-19 09:47:38 -0300

    changes

--- app/Http/Controllers/MensagemController.php
+++ app/Http/Controllers/MensagemController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class mensagemController extends Controller
 {
     public function param($nome, $comentario) {
-        return view('site.mensagem', ['nome' => $nome, 'comentario' => $comentario]);
+        //return view('site.mensagem', ['nome' => $nome, 'comentario' => $comentario]); //array associativo
+        return view('site.mensagem', compact('nome', 'comentario'));
     }
 }
