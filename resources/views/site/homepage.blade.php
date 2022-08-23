06b5a9de0756595ecd6615ed1f2014fa0dedf997
Isac Fernandes <isac.costa@fatec.sp.gov.br>
2022-08-23 13:53:55 -0300

    changes

--- resources/views/site/homepage.blade.php
+++ resources/views/site/homepage.blade.php
@@ -1,12 +1,15 @@
-<h3> Welcome to Homepage !!!</h3>
-<ul>
-    <li>
-        <a href="{{ route('site.index') }}"> Homepage </a>
-    </li>
-    <li>
-        <a href="{{ route('site.login') }}"> Login </a>
-    </li>
-    <li>
-        <a href="{{ route('app.admin') }}"> Admin </a>
-    </li>
-</ul>
+@extends('layouts.base')
+@section('main')
+    <h3> Welcome to Homepage !!!</h3>
+    <ul>
+        <li>
+            <a href="{{ route('site.index') }}"> Homepage </a>
+        </li>
+        <li>
+            <a href="{{ route('site.login') }}"> Login </a>
+        </li>
+        <li>
+            <a href="{{ route('app.admin') }}"> Admin </a>
+        </li>
+    </ul>
+@endsection
