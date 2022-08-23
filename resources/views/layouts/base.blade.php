85ebeb42dee775ed487e006e2c7c7fbbd32277b7
Isac Fernandes <isac.costa@fatec.sp.gov.br>
2022-08-23 15:43:13 -0300

    changes

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -16,7 +16,9 @@
     </style>
 </head>
 <body>
+    @yield('header')
     @yield('main')
+    @yield('footer')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
 </html>
--- resources/views/site/homepage.blade.php
+++ resources/views/site/homepage.blade.php
@@ -13,9 +13,14 @@
     </div>
 </nav>
 
-<section class="container"> 
+<section style="margin-left: 40px; margin-right: 40px;"> 
     <h3 calss="center-align"> Welcome to Homepage !!!</h3>
     <p> Bosons are the particles responsible for the interaction between fermions. Unlike them, these particles have no problem occupying the same place in space at the same time. They have integer spin, have no mass, and can have an electrical charge.
+        Bosons are the particles responsible for the interaction between fermions. Unlike them, these particles have no problem occupying the same place in space at the same time. They have integer spin, have no mass, and can have an electrical charge.
+        Bosons are the particles responsible for the interaction between fermions. Unlike them, these particles have no problem occupying the same place in space at the same time. They have integer spin, have no mass, and can have an electrical charge.
+    </p>
+    <p> Bosons are the particles responsible for the interaction between fermions. Unlike them, these particles have no problem occupying the same place in space at the same time. They have integer spin, have no mass, and can have an electrical charge.
+        Bosons are the particles responsible for the interaction between fermions. Unlike them, these particles have no problem occupying the same place in space at the same time. They have integer spin, have no mass, and can have an electrical charge.
         Bosons are the particles responsible for the interaction between fermions. Unlike them, these particles have no problem occupying the same place in space at the same time. They have integer spin, have no mass, and can have an electrical charge.
         Bosons are the particles responsible for the interaction between fermions. Unlike them, these particles have no problem occupying the same place in space at the same time. They have integer spin, have no mass, and can have an electrical charge.
     </p>
