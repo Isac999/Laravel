b73a2d8cbe0bab02eb34dd37f9a43d77086265c2
Isac Fernandes <isac.costa@fatec.sp.gov.br>
2022-09-02 09:23:35 -0300

    changes

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -7,14 +7,17 @@ use App\Models\Login;
 
 class LoginController extends Controller
 {
-    public function login(Request $request) {
+    public function login() {
+        return view('site.login');
+    }
+    public function insert(Request $request) {
         //print_r($request->all());
-        
+
         $login = new Login();
         $login->email = $request->input('email');
         $login->password = $request->input('password');
         $login->save();
-        
+
         return view('site.login');
     }
 }
