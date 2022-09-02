d35e70a122bb495f8e3605b7ba3f3eefe68fb85d
Isac Fernandes <isac.costa@fatec.sp.gov.br>
2022-09-02 09:20:49 -0300

    changes

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Login;
 
 class LoginController extends Controller
 {
     public function login(Request $request) {
-        print_r($request->all());
+        //print_r($request->all());
+        
+        $login = new Login();
+        $login->email = $request->input('email');
+        $login->password = $request->input('password');
+        $login->save();
+        
         return view('site.login');
     }
 }
