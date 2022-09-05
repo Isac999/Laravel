7610d10830f9b561d665298024eebedfdb5b40c2
Isac Fernandes <isac.costa@fatec.sp.gov.br>
2022-09-05 10:45:34 -0300

    changes

--- database/migrations/2022_08_24_122759_create_logins_table.php
+++ database/migrations/2022_08_24_122759_create_logins_table.php
@@ -19,6 +19,14 @@ return new class extends Migration
             $table->string('email', 45);
             $table->string('password', 45);
         });
+        /*
+        Realizando alter table
+        Schema::table('logins', function (Blueprint $table) {
+            //fazer o rollback
+            //$table->dropColumn(['tesNewColumn', 'param2', 'etc..']);
+            $table->dropColumn(['testNewColumn', 'testNewColumn02', 'testNewColumn03']);     
+        });
+        */
     }
 
     /**
